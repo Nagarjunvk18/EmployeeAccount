7de836fc78e9edbe6995c63f5538ed682210f56c
Nagarjuna Reddy Cheppala <arjun46214@gmail.com>
2023-01-13 13:52:34 +0530

    Updated testcases

--- SpringOneToOne/src/test/java/com/cg/service/EmployeeServiceImplTest.java
+++ SpringOneToOne/src/test/java/com/cg/service/EmployeeServiceImplTest.java
@@ -2,6 +2,9 @@ package com.cg.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -9,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -27,100 +31,96 @@ class EmployeeServiceImplTest {
 	@InjectMocks
 	private EmployeeServiceImpl eService;
 
+	Employee emp = new Employee();
+	List<Employee> empList = new ArrayList<Employee>();
+
 	@BeforeEach
 	public void setUp() {
 		// initialize mocks
 		eService = new EmployeeServiceImpl(empRepo);
 		MockitoAnnotations.openMocks(this);
+		Account acc = new Account();
+		acc.setAccNo(1234);
+		acc.setBankName("sbi");
+		acc.setAccBal(1000);
+		emp.setEid(1);
+		emp.setEname("kim");
+		emp.setEsal(19000);
+		emp.setEaccount(acc);
+
+		empList.add(emp);
 	}
 
 	// test method for adding an employee
 
 	@Test
+	@Order(1)
 	public void testAddEmployee() {
-
-		Account acc = new Account();
-		acc.setAccNo(123456);
-		acc.setBankName("sbi");
-		acc.setAccBal(1000);
-
-		Employee emp = new Employee();
-		emp.setEid(1);
-		emp.setEname("kim");
-		emp.setEsal(19000);
-		emp.setEaccount(acc);
-
 		// Here Saving the object to the mocked object
-		when(empRepo.save(emp)).thenReturn(emp);
-
-		// Here we have to check the actual method
-		Employee savedEmp = eService.addEmployee(emp);
-
-		assertEquals(savedEmp, emp);
+		when(eService.addEmployee(emp)).thenReturn(emp);
+		assertEquals(eService.addEmployee(emp), emp);
 	}
 
 	// test method for displaying the employeeList
 
 	@Test
+	@Order(2)
 	public void testGetAllEmployees() {
-		List<Employee> empList = new ArrayList<Employee>();
-		Account acc = new Account();
-		acc.setAccNo(123456);
-		acc.setBankName("sbi");
-		acc.setAccBal(1000);
-
-		Employee emp = new Employee();
-		emp.setEid(1);
-		emp.setEname("kim");
-		emp.setEsal(19000);
-		emp.setEaccount(acc);
-
-		Account acc2 = new Account();
-		acc.setAccNo(12345678);
-		acc.setBankName("hdfc");
-		acc.setAccBal(900);
-
-		Employee emp2 = new Employee();
-		emp.setEid(2);
-		emp.setEname("jim");
-		emp.setEsal(100000);
-		emp.setEaccount(acc2);
-		empList.add(emp);
-		empList.add(emp2);
-
-		when(empRepo.findAll()).thenReturn(empList);
+		when(eService.getAllEmployees()).thenReturn(empList);
 		assertEquals(eService.getAllEmployees(), empList);
-	}
-
-	// test the findEmpById method
-
-	@Test
-	public void testFindEmpById() throws NoSuchEmployeeFoundException {
-		Account acc = new Account();
-		acc.setAccNo(123456);
-		acc.setBankName("sbi");
-		acc.setAccBal(1000);
-
-		Employee emp = new Employee();
-		emp.setEid(1);
-		emp.setEname("kim");
-		emp.setEsal(19000);
-		emp.setEaccount(acc);
-
-		when(empRepo.findById(1)).thenReturn(Optional.of(emp));
-
-		assertEquals(eService.findEmpById(1), emp);
-
 	}
 
 	// test the findEmpById exception case
 
 	@Test
 	public void testFindEmpByIdException() {
-		when(empRepo.findById(1)).thenReturn(Optional.empty());
-		assertThrows(NoSuchEmployeeFoundException.class,()->{
-		eService.findEmpById(1);
+		try {
+			when(eService.findEmpById(1)).thenReturn(null);
+		} catch (NoSuchEmployeeFoundException e) {
+		}
+		assertThrows(NoSuchEmployeeFoundException.class, () -> {
+			eService.findEmpById(1);
 		});
+	}
+
+	// test the deleteEmpById
+
+	@Test
+	public void testDeleteEmpById() throws NoSuchEmployeeFoundException {
+		boolean isDeleted = eService.deleteEmployee(1);
+		verify(empRepo, times(1)).deleteById(1);
+		assertTrue(isDeleted);
+	}
+
+	@Test
+	public void testUpdateEmployee() {
+
+		emp.setEsal(29000);
+		try {
+			when(eService.updateEmp(1, emp)).thenReturn(emp);
+		} catch (NoSuchEmployeeFoundException e) {
+		}
+
+		try {
+			assertEquals(eService.updateEmp(1, emp), emp);
+		} catch (NoSuchEmployeeFoundException e) {
+
+		}
+	}
+
+	@Test
+	public void testFindEmpById() {
+		try {
+			when(eService.findEmpById(2)).thenReturn(emp);
+			assertEquals(emp,eService.findEmpById(1));
+		} catch (NoSuchEmployeeFoundException e) {
+		}
+	}
+
+	@Test
+	public void testGetEmpDetailsByAccNo() throws NoSuchEmployeeFoundException {
+	    when(eService.getEmpDetailsByAccNo(1234)).thenReturn(emp);
+	    assertEquals(eService.getEmpDetailsByAccNo(1234), null);
 	}
 
 }
